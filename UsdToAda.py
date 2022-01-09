b72e71f068da58c65cc0872a3ecfc140ceb81907
Zela <83733867+zellagui@users.noreply.github.com>
2022-01-09 14:13:36 -0500

    Update UsdToAda.py

--- UsdToAda.py
+++ UsdToAda.py
@@ -1,34 +1,41 @@
 from requests import Request, Session
 import json
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+url = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'
 
 parameters = {
-    'start':'1',
-    'limit':'10',
-    'convert':'USD'
+    'amount':1,
+    'id':'2010'
 }
 headers = {
     'Accepts': 'application/json',
     'X-CMC_PRO_API_KEY': '52da5bee-2bbc-49e0-b837-9544f2cacc60',
 }
 
-deleviry_cost = 20 #delivery cost
-
 session = Session()
 session.headers.update(headers)
 
-def get_price_ADA_USD(delevery_cost):
-    #Get method to access Data
+delivery_cost = 15 #delevery cost
+
+
+#Get delivery cost in USD as parameter and return delivery const in ADA
+def get_ADA_conv(delivery_cost):
     response = session.get(url, params=parameters, headers=headers)
     data = json.loads(response.text)
-    crypto = data["data"]
-    #Looping through all crypto to extract ADA Then the price in quote/usd
-    for x in crypto:
-        if x['symbol'] == 'ADA':
-            price = x['quote']['USD']['price']
-    return round(deleviry_cost / price, 2)
+    crypto = data["data"]['quote']['USD']['price']
+    cost_in_ADA =  round(delivery_cost / crypto, 2)
+    return cost_in_ADA
 
-print(get_price_ADA_USD(delivery_cost), 'ADA')
+try:
+    #Printing the the result
+    print(get_ADA_conv(delivery_cost), 'ADA')
 
-    
+except requests.exceptions.Timeout:
+    print('An error occurred')
+except requests.exceptions.TooManyRedirects:
+    print('An error occurred')
+except requests.exceptions.RequestException as e:
+    raise SystemExit(e)
+
+
+
